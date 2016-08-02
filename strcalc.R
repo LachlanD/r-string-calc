101686cb3b71bd445f4f461672717a24c98182fe
Lachlan <lachlan.d@gmail.com>
2016-08-02 14:38:07 +1000

    Add multiplication support

--- strcalc.R
+++ strcalc.R
@@ -7,7 +7,8 @@ compute = function(input_string) {
   num1 = as.integer(values[3])
   result = switch (operator, 
 			'+' = num0 + num1,
-			'-' = num0 - num1)
+			'-' = num0 - num1
+			'*' = num0 * num1)
   return(result)
   if (is.na(result)) {
     print("unknown operator!")
